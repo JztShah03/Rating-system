6da1904683570fa717deb671986641b9d0e3915e
JztShah03 <sdaud.ws@alice-smith.edu.my>
2026-05-17 21:42:21 +0800

    Remove SERVER, OTHER SERVICES, and OPERATING SYSTEM options, change NETWORKING to INTERNET

--- src/data/technicians.js
+++ src/data/technicians.js
@@ -3,12 +3,9 @@ export const technicians = [
   { name: 'AV / PROJECTOR', image: '/technician2.jpg' },
   { name: 'COMPUTER HARDWARE', image: '/technician3.jpg' },
   { name: 'EDU PLATFORM', image: '/technician4.jpg' },
-  { name: 'OTHER SERVICES', image: '/technician5.jpg' },
   { name: 'GOOGLE SERVICES', image: '/technician6.jpg' },
   { name: 'ID CARD CREATION', image: '/technician7.jpg' },
-  { name: 'NETWORKING', image: '/technician9.jpg' },
-  { name: 'OPERATING SYSTEM', image: '/technician10.jpg' },
-  { name: 'SERVER', image: '/technician11.jpg' },
+  { name: 'INTERNET', image: '/technician9.jpg' },
   { name: 'SOFTWARE APPLICATIONS', image: '/technician13.jpg' },
   { name: 'TABLET HADWARE', image: '/technician14.jpg' }
 ];
