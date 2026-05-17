30ec80626455d65522365318ab2b485d3d108148
JztShah03 <sdaud.ws@alice-smith.edu.my>
2026-05-17 21:50:20 +0800

    Add back EDU PLATFORM

--- src/data/technicians.js
+++ src/data/technicians.js
@@ -2,6 +2,7 @@ export const technicians = [
   { name: 'ACCOUNT', image: '/technician1.png' },
   { name: 'AV / PROJECTOR', image: '/technician2.jpg' },
   { name: 'COMPUTER', image: '/technician3.jpg' },
+  { name: 'EDU PLATFORM', image: '/technician4.jpg' },
   { name: 'GOOGLE SERVICES', image: '/technician6.jpg' },
   { name: 'ID CARD CREATION', image: '/technician7.jpg' },
   { name: 'INTERNET', image: '/technician9.jpg' },
