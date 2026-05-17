6b079d8d18bd8b8e051e32f5363caa2767fcc242
JztShah03 <sdaud.ws@alice-smith.edu.my>
2026-05-17 21:47:16 +0800

    Remove EDU PLATFORM, rename COMPUTER HARDWARE to COMPUTER, TABLET HADWARE to TABLET

--- src/data/technicians.js
+++ src/data/technicians.js
@@ -1,11 +1,10 @@
 export const technicians = [
   { name: 'ACCOUNT', image: '/technician1.png' },
   { name: 'AV / PROJECTOR', image: '/technician2.jpg' },
-  { name: 'COMPUTER HARDWARE', image: '/technician3.jpg' },
-  { name: 'EDU PLATFORM', image: '/technician4.jpg' },
+  { name: 'COMPUTER', image: '/technician3.jpg' },
   { name: 'GOOGLE SERVICES', image: '/technician6.jpg' },
   { name: 'ID CARD CREATION', image: '/technician7.jpg' },
   { name: 'INTERNET', image: '/technician9.jpg' },
   { name: 'SOFTWARE APPLICATIONS', image: '/technician13.jpg' },
-  { name: 'TABLET HADWARE', image: '/technician14.jpg' }
+  { name: 'TABLET', image: '/technician14.jpg' }
 ];
