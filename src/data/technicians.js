756a63c8a99efeab13c4f0fee7905dff469bf4be
JztShah03 <sdaud.ws@alice-smith.edu.my>
2026-05-19 09:53:17 +0800

    Rename technician labels

--- src/data/technicians.js
+++ src/data/technicians.js
@@ -1,11 +1,11 @@
 export const technicians = [
-  { name: 'ACCOUNT', image: '/technician1.png' },
-  { name: 'AV / PROJECTOR', image: '/technician2.jpg' },
-  { name: 'COMPUTER', image: '/technician3.jpg' },
-  { name: 'EDU PLATFORM', image: '/technician4.jpg' },
+  { name: 'ACCOUNT ISSUES', image: '/technician1.png' },
+  { name: 'AUDIO VISUAL / PROJECTOR', image: '/technician2.jpg' },
+  { name: 'COMPUTER HARDWARE', image: '/technician3.jpg' },
+  { name: 'EDU PLATFORM / WEBSITE', image: '/technician4.jpg' },
   { name: 'GOOGLE SERVICES', image: '/technician6.jpg' },
-  { name: 'ID CARD CREATION', image: '/technician7.jpg' },
-  { name: 'INTERNET', image: '/technician9.jpg' },
+  { name: 'ID CARD ISSUES', image: '/technician7.jpg' },
+  { name: 'INTERNET / WIFI', image: '/technician9.jpg' },
   { name: 'SOFTWARE APPLICATIONS', image: '/technician13.jpg' },
-  { name: 'TABLET', image: '/technician14.jpg' }
+  { name: 'MOBILE DEVICES / TABLET', image: '/technician14.jpg' }
 ];
